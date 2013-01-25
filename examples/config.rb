01dee0a1c6d5680790335eaa9e99a6870c8a860e
Abhay Jayant Kadam <abhaykadam88@gmail.com>
2013-01-25 19:54:39 +0530

    refectored the code

--- examples/config.rb
+++ examples/config.rb
@@ -1,9 +1,9 @@
 require 'em-ftpd'
 require File.dirname(__FILE__) + "/examples/driver.rb"
 
-AWS_KEY    = 'AKIAJ2IGHZQHCSJYT22A'
-AWS_SECRET = 'etpwFn9XnqWrfYKwrr2++++hgmEBFMxCvuBus/Xy'
-AWS_BUCKET = 'em-ftpd-trial-assignment1/abhay'
+AWS_KEY    = 'place your aws key here'
+AWS_SECRET = 'place your aws secrete key here'
+AWS_BUCKET = 'place your aws bucket name here'
 
 driver      Driver
 driver_args AWS_KEY, AWS_SECRET, AWS_BUCKET
